09be38854118e5a950df470c62942d2503ccf367
RyoWatanabe <wataryo0408@gmail.com>
2023-03-28 00:39:49 +0900

    crean

--- poke_app/lib/main.dart
+++ poke_app/lib/main.dart
@@ -24,53 +24,18 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      
-      
-      
-      
-      _counter++;
-    });
-  }
-
+class TopPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
-    return Scaffold(
-      
-    );
+
+    return Scaffold();
   }
+
+
 }
+
