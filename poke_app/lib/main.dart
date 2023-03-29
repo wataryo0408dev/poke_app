ed75d7c1a1542fb8cbc8cb2cb184e2de6d3ed890
RyoWatanabe <wataryo0408@gmail.com>
2023-03-29 21:03:31 +0900

    [fix] clear

--- poke_app/lib/main.dart
+++ poke_app/lib/main.dart
@@ -1,41 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:poke_app/scr_pokemon_list.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() {runApp(const MyApp());}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
         primarySwatch: Colors.blue,
       ),
-      home: TopPage(),
+      // home: TopPage(),
+      home: ScrPokemonList(),
     );
   }
 }
-
-class TopPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold();
-  }
-
-
-}
-
