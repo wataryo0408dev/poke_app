530daf93630b98efb4e0e61ab13f76079f119b3e
RyoWatanabe <wataryo0408@gmail.com>
2023-03-29 21:03:51 +0900

    [fix]clear

--- poke_app/lib/poke_api.dart
+++ poke_app/lib/poke_api.dart
@@ -1,37 +1,13 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 class Pokemon{
   final String name;
+  final int id;
   final String imageUrl;
-  final int height;
-  final int weight;
 
   Pokemon({
     required this.name,
+    required this.id,
     required this.imageUrl,
-    required this.height,
-    required this.weight
   });
-
-  factory Pokemon.fromJson(Map<String, dynamic> json){
-    return Pokemon(
-      name: json['name'],
-      imageUrl: json['sprites']['front_default'],
-      height: json['height'],
-      weight: json['weight'],
-    );
-
-  }
 }
 
-Future<Pokemon> fetchPokemon(String name) async{
-  final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$name'));
-  if(response.statusCode == 200){
-    final json = jsonDecode(response.body);
-    return Pokemon.fromJson(json);
-
-  }else{
-    throw Exception('データの取得に失敗しました。');
-  }
-}
