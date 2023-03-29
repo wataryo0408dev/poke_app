ba325a3d61024a4f7c5e5fb69e8683388f09b157
RyoWatanabe <wataryo0408@gmail.com>
2023-03-29 21:04:31 +0900

    [add]pokemonList

---
+++ poke_app/lib/scr_pokemon_list.dart
@@ -0,0 +1,77 @@
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/retry.dart';
+import 'package:poke_app/poke_api.dart';
+import 'package:http/http.dart' as http;
+
+class ScrPokemonList extends StatefulWidget{
+  @override
+  ScrPokemonListState createState() => ScrPokemonListState();
+}
+class ScrPokemonListState extends State<ScrPokemonList>{
+
+  Future<Map<String, dynamic>> getPokemonSpecies(String url) async{
+    final response = await http.get(Uri.parse(url));
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  }
+
+Future<List<Pokemon>> getPokemonList() async{
+  final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=151'));
+  final pokemonListJson = jsonDecode(response.body)['results'] as List<dynamic>;
+  final pokemonList = <Pokemon>[];
+  for (final pokemon in pokemonListJson) {
+    final speciesUrl = (await getPokemonSpecies(pokemon['url']))['species']['url'] as String;
+    final speciesData = await getPokemonSpecies(speciesUrl);
+    final name = speciesData['names'].firstWhere((data) => data['language']['name'] == 'ja')['name'] as String;
+    final id = speciesData['id'];
+    final imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonListJson.indexOf(pokemon) + 1}.png';
+    pokemonList.add(Pokemon(
+      name: name,
+      id: id,
+      imageUrl: imageUrl,
+    ));
+  }
+  return pokemonList;
+}
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ポケモンリスト'),
+      ),
+      body: FutureBuilder<List<Pokemon>>(
+        future: getPokemonList(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final pokemon = snapshot.data![index];
+                return ListTile(
+                  leading: Image.network(pokemon.imageUrl),
+                  title: Text(pokemon.name),
+                  trailing: const Icon(Icons.arrow_forward),
+                );
+              },
+            );
+          }else{
+            return Center(
+              // child: CircularProgressIndicator(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const[
+                  CircularProgressIndicator(),
+                  Padding(padding: EdgeInsets.all(10)),
+                  Text("データを取得中です")
+                ],
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
