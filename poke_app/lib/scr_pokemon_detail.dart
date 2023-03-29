bc6d04d233992607a12941d1315f74d712d552f8
RyoWatanabe <wataryo0408@gmail.com>
2023-03-29 22:07:46 +0900

    Create scr_pokemon_detail.dart

---
+++ poke_app/lib/scr_pokemon_detail.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class ScrPokemonDetail extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Detail"),
+      ),
+    );
+  }
+}
